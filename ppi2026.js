e5e1c0035ab1ef35251a5a75fc6fe70f6d211638
iohanportela <iohan.portela@escolar.ifrn.edu.br>
2026-05-07 10:37:30 +0000

    Pending changes exported from your codespace

---
+++ ppi2026.js
@@ -0,0 +1,34 @@
+// a
+/*
+a
+a
+a
+*/
+
+
+
+console.log('lanelus');
+
+// variaveis
+let lanelus = 'slassesles';
+let nu = 28.4;
+let isGiria = true;
+
+const DNAS = 4;
+
+let x = '5'+ 5;
+let y = 'asdasd'+123;
+console.log(x);
+console.log(y);
+console.log(DNAS);
+
+console.log(typeof lanelus);
+console.log(nu**2);
+console.log(typeof isGiria);
+
+console.log('5'==5);
+console.log('5'=='5');
+console.log('asd'=='5');
+console.log('5'===5);
+
+console.log(++x);
